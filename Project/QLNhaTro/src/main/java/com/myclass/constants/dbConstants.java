9d05dc23f0ed2b856148e47841c5e60b879f1a8a
Dinh Trung <dvtrung99@gmail.com>
2021-06-17 11:01:52 +0700

    Add project

--- Project/QLNhaTro/src/main/java/com/myclass/constants/dbConstants.java
+++ Project/QLNhaTro/src/main/java/com/myclass/constants/dbConstants.java
@@ -3,6 +3,6 @@ package com.myclass.constants;
 public class DBConstants {
 	 public final static String URL = "jdbc:mysql://localhost:3306/qlnhatro"; 
 	 public final static String USERNAME = "root"; 
-	 public final static String PASSWORD = "phongpham98"; 
+	 public final static String PASSWORD = "Trung@123"; 
 	 public final static String DRIVER = "com.mysql.cj.jdbc.Driver"; 
 }
